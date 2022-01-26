76c9dc8d6944e0ccc85424deb8fb20ca47578127
Gustavo Kazuo <kazucalado@gmail.com>
2022-01-26 14:45:22 -0300

    Projeto criado

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+import { Button, ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+function App() {
+  return (
+    <Button variant="contained" color="success">
+      Success
+    </Button>
+  );
+}
+
+export default App;
