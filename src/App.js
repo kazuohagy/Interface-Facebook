877212fd099312cf20ce00b56fcfb77a9d2732c9
Gustavo Kazuo <kazucalado@gmail.com>
2022-01-26 15:47:18 -0300

    Deu certo Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import reactDom from "react-dom";
 import { Button } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#FF69B4",
+      },
+      secondary: {
+        main: "#FF0000",
+      },
+    },
+  });
   return (
-    <Button variant="contained" color="primary">
-      Pasdasdasd
-    </Button>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="secondary">
+        Click para jogar
+      </Button>
+    </ThemeProvider>
   );
 }
 
