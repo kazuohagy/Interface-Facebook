bbbb89cb58dcea2059a691d55affb98109c272b2
Gustavo Kazuo <kazucalado@gmail.com>
2022-02-02 17:09:57 -0300

    Facebook app bar

--- src/Home.js
+++ src/Home.js
@@ -16,6 +16,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import HomeIcon from "@mui/icons-material/Home";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+import SearchIcon from "@mui/icons-material/Search";
 import {
   AppBar,
   Button,
@@ -63,6 +64,7 @@ const useStyles = makeStyles((theme) => ({
   },
   logo: {
     height: 50,
+    paddingRight: 25,
   },
   listItemText: {
     fontSize: 14,
@@ -74,88 +76,11 @@ const useStyles = makeStyles((theme) => ({
   subheader: {
     textTransform: "uppercase",
   },
+  espacamento: {
+    padding: 25,
+  },
 }));
-const videos = [
-  {
-    id: 1,
-    title:
-      "FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29",
-    channel: "Lucas Nhimi",
-    views: "11 mi de visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb1.png",
-  },
-  {
-    id: 2,
-    title:
-      "COMO MELHORAR SEU CODIGO JAVASCRIPT (ESLINT + PRETTIER + EDITORCONFIG) | Dicas e Truques #02",
-    channel: "Lucas Nhimi",
-    views: "957 mil visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb2.png",
-  },
-  {
-    id: 3,
-    title:
-      "CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27",
-    channel: "Lucas Nhimi",
-    views: "106 mil visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb3.png",
-  },
-  {
-    id: 4,
-    title:
-      "CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27",
-    channel: "Lucas Nhimi",
-    views: "5,6 mi de visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb4.png",
-  },
-  {
-    id: 5,
-    title:
-      "EDITOR DE POST COM MARKDOWN 2 | Criando uma Rede Social com React.js e .NET Core #26",
-    channel: "Lucas Nhimi",
-    views: "2,2 mi de visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb5.png",
-  },
-  {
-    id: 6,
-    title: "COMO MIGRAR PARA REACT HOOKS | Dicas e Truques #01",
-    channel: "Lucas Nhimi",
-    views: "233 mil visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb6.png",
-  },
-  {
-    id: 7,
-    title:
-      "PRÉ-REQUISITOS | Criando uma Rede Social com React.js e .NET Core #01",
-    channel: "Lucas Nhimi",
-    views: "118 mil visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb7.png",
-  },
-  {
-    id: 8,
-    title:
-      "GIT E GITHUB | Criando uma Rede Social com React.js e .NET Core #04",
-    channel: "Lucas Nhimi",
-    views: "1,9 mi de visualizações",
-    date: "há 1 semana",
-    avatar: "/images/avatar.jpeg",
-    thumb: "/images/thumb8.png",
-  },
-];
+
 export default function Home({ darkMode, setDarkMode }) {
   const classes = useStyles();
   const theme = useTheme();
@@ -163,15 +88,18 @@ export default function Home({ darkMode, setDarkMode }) {
     <div className={classes.root}>
       <AppBar color="inherit" sx={{ boxShadow: 0 }} className={classes.appBar}>
         <Toolbar>
-          <IconButton edge="start" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-
           <img
             src={"/images/facebookIcon.png"}
             alt="logo"
             className={classes.logo}
           ></img>
+          <IconButton
+            edge="start"
+            aria-label="menu"
+            className={classes.espacamento}
+          >
+            <SearchIcon />
+          </IconButton>
 
           <div className={classes.grow}></div>
           <Switch
